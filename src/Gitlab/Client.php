7b437db3f84686404b1cd112348e0d7b12a3ad67
Hendrik Pilz <hepisec@gmail.com>
2023-07-23 18:01:16 +0200

    Added Client::getLabels()

--- src/Gitlab/Client.php
+++ src/Gitlab/Client.php
@@ -23,11 +23,11 @@ class Client {
         $this->token = $token;
     }
 
-    public function getGroup(string $group) {
+    public function getGroup(string $group): array|bool {
         return $this->get('/groups/' . $group);
     }
 
-    public function createGroup(string $name, string $path) {
+    public function createGroup(string $name, string $path): array|bool {
         return $this->post('/groups', [
                     'name' => $name,
                     'path' => $path,
@@ -36,47 +36,51 @@ class Client {
         ]);
     }
 
-    public function addGroupMember(string $group, int $memberId, int $accessLevel) {
+    public function addGroupMember(string $group, int $memberId, int $accessLevel): array|bool {
         return $this->post('/groups/' . $group . '/members', [
                     'user_id' => $memberId,
                     'access_level' => $accessLevel
         ]);
     }
 
-    public function findGroupProject(string $group, string $project) {
+    public function findGroupProject(string $group, string $project): array|bool {
         return $this->get('/groups/' . $group . '/projects', [
                     'search' => $project
         ]);
     }
 
-    public function getIssues(int $projectId, $page = 1, $perPage = 20) {
+    public function getIssues(int $projectId, $page = 1, $perPage = 20): array|bool {
         return $this->get('/projects/' . $projectId . '/issues', [
                     'page' => $page,
                     'per_page' => $perPage
         ]);
     }
 
-    public function findIssue(int $projectId, $title) {
+    public function findIssue(int $projectId, $title): array|bool {
         return $this->get('/projects/' . $projectId . '/issues', [
                     'search' => $title
         ]);
     }
 
-    public function createIssue(int $projectId, array $issueData) {
+    public function createIssue(int $projectId, array $issueData): array|bool {
         return $this->post('/projects/' . $projectId . '/issues', $issueData);
     }
 
-    public function updateIssue(int $projectId, int $issueIid, array $issueData) {
+    public function updateIssue(int $projectId, int $issueIid, array $issueData): array|bool {
         return $this->put('/projects/' . $projectId . '/issues/' . $issueIid, $issueData);
     }
 
-    public function addTimeSpent(int $projectId, int $issueIid, string $timeSpent) {
+    public function getLabels(int $projectId): array|bool {
+        return $this->get('/projects/' . $projectId . '/labels');
+    }
+
+    public function addTimeSpent(int $projectId, int $issueIid, string $timeSpent): array|bool {
         return $this->post('/projects/' . $projectId . '/issues/' . $issueIid . '/add_spent_time', [
                     'duration' => $timeSpent
         ]);
     }
 
-    public function createIssueComment(int $projectId, int $issueIid, string $comment, ?\DateTimeInterface $createdAt = null) {
+    public function createIssueComment(int $projectId, int $issueIid, string $comment, ?\DateTimeInterface $createdAt = null): array|bool {
         if ($createdAt === null) {
             $createdAt = new \DateTime();
         }
@@ -87,7 +91,7 @@ class Client {
         ]);
     }
 
-    public function uploadAttachment(int $projectId, string $filePath) {
+    public function uploadAttachment(int $projectId, string $filePath): array|bool {
         $formFields = [
             'file' => DataPart::fromPath($filePath),
         ];
@@ -100,18 +104,18 @@ class Client {
         ]);
     }
 
-    public function getNamespace($name) {
+    public function getNamespace($name): array|bool {
         return $this->get('/namespaces/' . $name);
     }
 
-    public function getProjects($page = 1, $perPage = 20) {
+    public function getProjects($page = 1, $perPage = 20): array|bool {
         return $this->get('/projects', [
                     'page' => $page,
                     'per_page' => $perPage
         ]);
     }
 
-    public function createProject(int $namespaceId, string $path) {
+    public function createProject(int $namespaceId, string $path): array|bool {
         return $this->post('/projects', [
                     'path' => $path,
                     'visibility' => 'private',
@@ -119,29 +123,29 @@ class Client {
         ]);
     }
 
-    public function searchUsers($query) {
+    public function searchUsers($query): array|bool {
         return $this->get('/users', [
                     'search' => $query
         ]);
     }
 
-    private function getUrl($url) {
+    private function getUrl($url): string {
         return $this->baseUrl . '/api/v4' . $url;
     }
 
-    private function get($url, array $params = [], array $options = []) {
+    private function get($url, array $params = [], array $options = []): array|bool {
         return $this->request('GET', $this->getUrl($url), array_merge($options, ['query' => $params]));
     }
 
-    private function post($url, array $params = [], array $options = []) {
+    private function post($url, array $params = [], array $options = []): array|bool {
         return $this->request('POST', $this->getUrl($url), array_merge($options, ['body' => $params]));
     }
 
-    private function put($url, array $params = [], array $options = []) {
+    private function put($url, array $params = [], array $options = []): array|bool {
         return $this->request('PUT', $this->getUrl($url), array_merge($options, ['body' => $params]));
     }
 
-    private function request($method, $url, array $options = []) {
+    private function request($method, $url, array $options = []): array|bool {
         $options = array_merge_recursive($options, [
             'headers' => [
                 'PRIVATE-TOKEN' => $this->token
